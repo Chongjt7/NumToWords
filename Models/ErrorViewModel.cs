6b58b0ff893b55f1673e3c23dfa1f8a55e32f7c9
chongjt7 <chongjt7@gmail.com>
2024-04-11 20:04:04 +0800

    Test commit from desktop

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NumsToWord.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
